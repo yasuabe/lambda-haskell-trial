db154d801f1c7ad3028d05c3a9d4c8abe01d44ec
Yasuyuki Abe <yasu@yasuabe.net>
2019-06-23 12:54:12 +0900

    add simple-slash-command

---
+++ simple-slash-command/README.md
@@ -0,0 +1,1 @@
+# simple-slash-command
---
+++ simple-slash-command/src/Lib.hs
@@ -0,0 +1,19 @@
+module Lib where
+
+import GHC.Generics
+import Aws.Lambda.Runtime
+import Data.Aeson
+
+data Person = Person
+  { personName :: String
+  , personAge :: Int
+  } deriving (Generic)
+instance FromJSON Person
+instance ToJSON Person
+
+handler :: Person -> Context -> IO (Either String Person)
+handler person context =
+  if personAge person > 0 then
+    return (Right person)
+  else
+    return (Left "A person's age must be positive")
